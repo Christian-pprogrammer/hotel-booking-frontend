8412a119acbebf960bd2382713782b1f0a24aa40
MPANO Christian <mpanoc6@gmail.com>
2022-02-05 15:48:58 +0200

    added login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../services/auth.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private auth: AuthService) { }
+  form = this.fb.group({
+    username: this.fb.control('', [Validators.required]),
+    password: this.fb.control('', [Validators.required])
+  });
+  get username() {
+    return this.form.get('username');
   }
 
+  get password() {
+    return this.form.get('password');
+  }
+
+  submit() {
+    this.auth.login(this.form.value);
+  }
 }
