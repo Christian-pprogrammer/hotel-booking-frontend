a0aaef55ed68db824614bafceffa160cfc92988a
MPANO Christian <mpanoc6@gmail.com>
2022-02-08 06:10:43 +0200

    added create-room component with some changes

--- src/app/components/create-room/create-room.component.ts
+++ src/app/components/create-room/create-room.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-room',
   templateUrl: './create-room.component.html',
-  styleUrls: ['./create-room.component.css']
+  styleUrls: ['./create-room.component.css'],
 })
-export class CreateRoomComponent implements OnInit {
+export class CreateRoomComponent {
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  form = this.fb.group({
+    roomNumber: this.fb.control('', [Validators.required]),
+    floorNumber: this.fb.control('', [Validators.required]),
+    roomImage: this.fb.control('', [Validators.required]),
+    price: this.fb.control('', [Validators.required]),
+  });
 
-  ngOnInit(): void {
+  get roomNumber() {
+    return this.form.get('roomNumber');
   }
-
+  get floorNumber() {
+    return this.form.get('floorNumber');
+  }
+  get roomImage() {
+    return this.form.get('roomImage');
+  }
+  get price() {
+    return this.form.get('price');
+  }
+  submit() {
+    console.log(this.form.value)
+  }
 }
