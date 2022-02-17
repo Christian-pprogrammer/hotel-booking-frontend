a124eddf8d92f28898fd060134a8986edf03c262
Christian-pprogrammer <mpanoc6@gmail.com>
2022-02-17 20:32:27 +0200

    added create-hall component

--- src/app/components/create-hall/create-hall.component.ts
+++ src/app/components/create-hall/create-hall.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { CustomValidators } from 'src/app/common/CustomValidator';
 
 @Component({
   selector: 'app-create-hall',
@@ -10,7 +11,12 @@ export class CreateHallComponent {
 
   constructor(private fb: FormBuilder) { }
   form: any = this.fb.group({
-    hallNumber: this.fb.control('', [Validators.required])
-  })
-
+    hallNumber: this.fb.control('', [Validators.required, CustomValidators.numeric]),
+    floorNumber: this.fb.control('', [Validators.required, CustomValidators.numeric]),
+    floorImage: this.fb.control('', [Validators.required]),
+    price: this.fb.control('', [Validators.required])
+  }) 
+  submit() {
+    console.log('hello submission')
+  }
 }
