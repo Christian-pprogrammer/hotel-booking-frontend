8ed80d45f47f4e5c935e72274e682fbb471c7b41
Christian-pprogrammer <mpanoc6@gmail.com>
2022-02-19 17:58:44 +0200

    added error handler for image cloudinary upload

--- src/app/services/rooms.service.ts
+++ src/app/services/rooms.service.ts
@@ -1,5 +1,7 @@
+import { catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,16 +9,24 @@ import { Injectable } from '@angular/core';
 export class RoomsService {
 
   constructor(private http:HttpClient) { }
+  url = 'http://localhost:5000/'
   createRoom(payload: object, fileData: any) {
     let formData = new FormData();
     formData.append("file", fileData);
     formData.append("upload_preset", "kqznhjvq");
     let cloudinary_url="https://api.cloudinary.com/v1_1/rwanda-coding-academy";
     this.http.post(cloudinary_url+"/image/upload", formData)
+      .pipe(
+        catchError((err: any )=> {
+          if(err.status == 400) {
+            return throwError(new Error("unknown error"));
+          }else{
+            return throwError(new Error("unknown error"));
+          }
+        })
+      )
       .subscribe(res=>{
-        console.log(res);
-      }, err=>{
-        console.log(err)
+        this.http.post()
       })
   }
 }
